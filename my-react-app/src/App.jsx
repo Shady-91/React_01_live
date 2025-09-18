e1c70dc19a8bb2496a92a8a78ed298223bc37f11
Shady-91 <Shady_91@hotmail.it>
2025-09-18 21:03:08 +0200

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import AppHeader from './components/AppHeader';
+
+
+function App() {
+
+
+  return (
+    <>
+      <AppHeader></AppHeader>
+    </>
+  )
+}
+
+export default App
---
+++ my-react-app/src/components/AppHeader.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+      return (
+            <header>
+                  <h1>React</h1>
+                  <h2>is a Power</h2>
+            </header>
+      );
+}
