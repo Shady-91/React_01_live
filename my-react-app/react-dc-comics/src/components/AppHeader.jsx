36f8651640aaab6a9833848902a08a0ad845b73a
Shady-91 <Shady_91@hotmail.it>
2025-09-25 11:39:35 +0200

    finish Header

--- my-react-app/react-dc-comics/src/components/AppHeader.jsx
+++ my-react-app/react-dc-comics/src/components/AppHeader.jsx
@@ -1,7 +1,25 @@
+import logo from '../assets/img/dc-logo.png';
+
 function AppHeader() {
       return (
             <header>
-                  <p>© 2025 DC Comics. Tutti i diritti riservati.</p>
+                  <div className="header-top">
+                        <img src={logo} alt="DC Logo" className="logo" />
+                        <nav>
+                              <ul className="nav-list">
+                                    <li>CHARACTERS</li>
+                                    <li>COMICS</li>
+                                    <li>MOVIES</li>
+                                    <li>TV</li>
+                                    <li>GAMES</li>
+                                    <li>COLLECTIBLES</li>
+                                    <li>VIDEOS</li>
+                                    <li>FANS</li>
+                                    <li>NEWS</li>
+                                    <li>SHOP</li>
+                              </ul>
+                        </nav>
+                  </div>
             </header>
       );
 }
